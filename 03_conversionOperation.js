59cde401f0c78f4e444eb927827048957a53ca8f
devsoni-20 <devsoni4545@gmail.com>
2023-12-20 13:24:03 +0530

    adding operation

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -1,10 +1,10 @@
 let score = '33abc';
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -12,12 +12,32 @@ console.log(valueInNumber);
 
 let isLoggedIn = "dev";
 let booleanisLoggedIn = Boolean(isLoggedIn);
-console.log(booleanisLoggedIn);
+// console.log(booleanisLoggedIn);
 
 // 0 => false and "" => false
 // 1 => true and "dev" = true
 
 let someNumber = 33
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+// ******************* Operation ************************
+
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = 'Dev';
+let str2 = 'Soni'
+
+let str3 = str1+' '+str2;
+// console.log(str3);
+
+// console.log("1" +2);
+// console.log(1 + "2");
+// console.log("1" + 2+ 2);
+// console.log(1 + 2 + "2")
+
