2a0332466dc338be88356d3fc1b924e936f924a3
devsoni-20 <devsoni4545@gmail.com>
2023-12-24 18:32:20 +0530

    adding 06_numsAndMaths file

---
+++ 06_numsAndMaths.js
@@ -0,0 +1,24 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 123.7567;
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ======================== Maths ========================
+// console.log(Math);
+// console.log(Math.abs(-2));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.4));
+// console.log(Math.floor(4.4));
+// console.log(Math.min(3,4,5,6));
+// console.log(Math.max(3,4,5,6));
+
+console.log(Math.random()*10+1);
