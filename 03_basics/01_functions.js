75c10fe901a484d013d2690cf488f45002cc806c
devsoni-20 <devsoni4545@gmail.com>
2023-12-26 15:50:48 +0530

    changing some code

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -27,4 +27,32 @@ function loginUserMessage(userName)
     if(userName == undefined){return `Nothing is passed`;}
     return `${userName} just logged in`;
 }
-console.log(loginUserMessage());
+// console.log(loginUserMessage());
+
+function calculateCartPrice(...num1)
+{
+    return num1;
+}
+// console.log(calculateCartPrice(100, 200, 300, 400));
+
+const user = {
+    username: 'Dev',
+    price: 199
+}
+
+function handleObject(anyObject)
+{
+    console.log(`username is ${anyObject.username} and price is ${anyObject.price}`);
+}
+// handleObject(user);
+handleObject({
+    username: 'sam',
+    price: 200
+})
+
+const newArray = [100, 200, 300, 400];
+function returnSecondValue(getArray)
+{
+    return getArray[1];
+}
+console.log(returnSecondValue(newArray));
