1bdf2a637ec02960835ae281a7c296bf09b95cc9
devsoni-20 <devsoni4545@gmail.com>
2023-12-28 23:48:09 +0530

    adding two.js file

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let index = 0;
+// while(index < 10)
+// {
+//         console.log(`Value of index is ${index++}`);
+//         index++;
+// }
+
+let arr = [1,2,3,4,5];
+let i=0
+// console.log(arr[5]);
+// while(i < arr.length)
+// {
+//     console.log(`Value of i is ${i}`);
+//     i++;
+// }
+let score = 1;
+do{
+    console.log(`Score is ${score}`);
+    score++;
+}while(score <= 10);
